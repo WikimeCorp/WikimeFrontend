2b91a1d8c13a9931b72a664d7fb2f474220613ee
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2022-10-18 23:53:12 +0300

    Article page (#10)
    * comment layout
    
    * created comment component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import ArticleTable from './components/ArticleTable';
 import Navbar from './components/UI/Navbar/Navbar';
 import ArticleList from './components/ArticleList';
 import List from './pages/List';
+import Article from './pages/Article';
 
 const App = () => {
   return (
-    <List />
+    <Article />
   );
 };
 
