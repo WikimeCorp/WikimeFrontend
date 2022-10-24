9562354fc9c986584316953e58df5dce38d2fe70
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2022-10-25 02:49:13 +0300

    added landing

---
+++ src/pages/Main.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import "../styles/Main.css";
+import art from "../styles/img/Art.png";
+import MainButton from '../components/UI/button/main/MainButton';
+import { useNavigate } from 'react-router-dom';
+
+const Main: FC = () => {
+    const navigate = useNavigate();
+
+    return(
+        <div className='Main-page'>
+            <div className='landing'>
+                <div className='landing-anime'>
+                    <img src={art}/>
+                    <span>Мастера Меча Онлайн: Прогрессив — Ария в беззвёздной ночи</span>
+                </div>
+                <div className='landing-info'>
+                    <h1>Онлайн энциклопедия аниме</h1>
+                    <MainButton onClick={() => navigate('/list')}>Перейти к статьям</MainButton>
+                </div>
+            </div>
+            <div className='popular'>
+                <h1>Популярное</h1>                                
+            </div>
+        </div>
+    );
+};
+
+export default Main;
