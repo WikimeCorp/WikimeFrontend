03b2921c1dd22da94d1df14578a465bd47317394
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2022-11-08 00:08:10 +0300

    api post anime

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,13 +1,20 @@
 import TextForm from "../components/UI/Form/TextForm";
 import { useAppDispatch } from "../hooks/redux";
+import { useAddAnimeMutation } from "../services/anime";
 import { clearGenres } from "../store/reducers/GenresSlice";
 import "../styles/Add.css";
 import { FormTextFields } from "../types/FormTextFields";
 
 const Add = () => {
 
-    const onSubmit = (formFields: FormTextFields) => {
-        console.log(formFields);
+    const [addArticle, { isError }] = useAddAnimeMutation();
+    const dispatch = useAppDispatch();
+
+    const onSubmit = async (formFields: FormTextFields) => {
+        if(formFields) {
+            await addArticle(formFields).unwrap();
+            dispatch(clearGenres());
+        }
     };
 
     return (
