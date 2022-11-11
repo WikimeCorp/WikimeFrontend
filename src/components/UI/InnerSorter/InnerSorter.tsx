6bf82bd6c97214dc029849d99449f951906fb7a0
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2022-11-11 20:46:29 +0300

    added chooseSort

--- src/components/UI/InnerSorter/InnerSorter.tsx
+++ src/components/UI/InnerSorter/InnerSorter.tsx
@@ -1,18 +1,48 @@
 import cl from "./InnerSorter.module.css"
 import ViewButton from "../button/view/ViewButtons";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { selectSort } from "../../../store/reducers/ListSlice";
 
 const InnerSorter = () => {
+
+    const Label: string[] = [ "popular", "new", "rate", "date" ];
+    const active = useAppSelector(state => state.listReducer.sort);
+    const dispatch = useAppDispatch();
 
     return(
     <div className={cl.innerSorter}>
         <div className={cl.content}>
             <div className={cl.catalog}>
                 <div>
-                    <span>Сортировать по:</span>
-                    <a href='#'>Популярности</a>
-                    <a href='#'>Обновлению</a>                    
-                    <a href='#'>Рейтингу</a>
-                    <a href='#'>Дате выхода</a>
+                    <span className={cl.catalogSort}>Сортировать по:</span>
+                    <button 
+                        onClick={() => dispatch(selectSort(Label[0]))}
+                        className={active === Label[0] ? 
+                            cl.btnSortActive : cl.btnSort}
+                    >
+                        Популярности
+                    </button>
+                    <button 
+                        onClick={() => dispatch(selectSort(Label[1]))}
+                        className={active === Label[1] ? 
+                            cl.btnSortActive : cl.btnSort}
+                    >
+                        Обновлению
+                    </button>
+                    <button 
+                        onClick={() => dispatch(selectSort(Label[2]))}
+                        className={active === Label[2] ? 
+                            cl.btnSortActive : cl.btnSort}
+                    >
+                        Рейтингу
+                    </button>
+                    <button 
+                        onClick={() => dispatch(selectSort(Label[3]))}
+                        className={active === Label[3] ? 
+                            cl.btnSortActive : cl.btnSort}
+                    >
+                        Дате выхода
+                    </button>
                 </div>                
                 <ViewButton />               
             </div>            
